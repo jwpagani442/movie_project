0d4f0b41a2a426b1f6e42fec3b416fa95a19cf66
jwpagani442 <11468144+jwpagani442@users.noreply.github.com>
2017-09-21 20:53:46 -0500

    Update media.py

--- media.py
+++ media.py
@@ -2,7 +2,7 @@ import webbrowser
 
 class Movie():
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-        """Initialize class variables."""
+        """Construcutor used to initialize class variables."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
